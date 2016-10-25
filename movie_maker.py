707903151b6a537028f31b7f5beb4d55df114738
Alon Stern <sternalon@gmail.com>
2016-10-25 16:42:02 -0400

    a few changes

--- movie_maker.py
+++ movie_maker.py
@@ -66,6 +66,11 @@ def main():
         input_filename='/home/aas/Iceberg_Project/iceberg_scripts/python_scripts/movies/00010101.icebergs_day.nc'
         output_filename='test5.mp4'
 
+	#General flags
+	horizontal_movie=True
+	cross_section_movie=False
+
+
 	Shelf_path='/lustre/f1/unswept/Alon.Stern/MOM6-examples_Alon/ice_ocean_SIS2/Tech_ISOMIP/Shelf/Melt_on_without_decay_with_spreading_trimmed_shelf/'
 	Berg_path='/lustre/f1/unswept/Alon.Stern/MOM6-examples_Alon/ice_ocean_SIS2/Tech_ISOMIP/Bergs/Melt_on_without_decay_with_spreading_trimmed_shelf/'
 
@@ -90,17 +95,50 @@ def main():
         ################################  Plotting melt comparison  ##########################################################
         ######################################################################################################################
 
-	filename=Berg_ocean_file
-	print filename
-        field_name='mass_berg'
-        #field_name='u'
-	direction='xy'
-	data=load_and_compress_data(filename,field=field_name,time_slice='all',time_slice_num=-1,rotated=rotated, direction=direction ,dir_slice=None, dir_slice_num=20)
-	print data.shape
+	if horizontal_movie is True:
 
-        #Loading data from NC file.
-        #[field,x,y]=load_data_from_nc_file(input_filename,field_name)
+		filename=Berg_ocean_file
+		print filename
+		field_name='mass_berg'
+		#field_name='u'
+		direction='xy'
+		data=load_and_compress_data(filename,field=field_name,time_slice='all',time_slice_num=-1,rotated=rotated, direction=direction ,dir_slice=None, dir_slice_num=20)
+		print data.shape
 
+		#Loading data from NC file.
+		#[field,x,y]=load_data_from_nc_file(input_filename,field_name)
+
+
+        ######################################################################################################################
+        ################################  Plotting Cross Section      ########################################################
+        ######################################################################################################################
+
+
+        if cross_section_movie is True:
+                plot_anomaly=True
+                time_slice='mean'
+                #vertical_coordinate='z'
+                vertical_coordinate='layers'  #'z'
+                #field='temp'  ; vmin=-2.0  ; vmax=1.0  ;vdiff=0.1   ; vanom=0.3
+                field='salt'  ; vmin=34  ; vmax=34.7  ;vdiff=0.02  ; vanom=0.02
+                #field='v'  ; vmin=-0.01  ; vmax=0.01  ;vdiff=0.01  ; vanom=0.01
+                #field='v'  ; vmin=-0.01  ; vmax=0.01  ;vdiff=0.01  ; vanom=0.01
+                filename1=Berg_ocean_file
+                
+                if vertical_coordinate=='z':
+                        filename1=filename1.split('.nc')[0] + '_z.nc'
+                if rotated is True:
+                        direction='yz'
+                        dist=yvec
+                else:
+                        direction='xz'
+                        dist=xvec
+
+                data1=load_and_compress_data(filename1,field , time_slice, time_slice_num=-1, direction=direction ,dir_slice=None, dir_slice_num=20,rotated=rotated)
+                elevation1 = get_vertical_dimentions(filename1,vertical_coordinate, time_slice, time_slice_num=-1, direction=direction ,dir_slice=None, dir_slice_num=20,rotated=rotated)
+                (y1 ,z1 ,data1) =interpolated_onto_vertical_grid(data1, elevation1, dist, vertical_coordinate)
+
+		print data.shape
 
 
         ani_frame(data,output_filename)
