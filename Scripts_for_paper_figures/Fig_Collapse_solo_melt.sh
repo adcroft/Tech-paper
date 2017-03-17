7292e5f504cad78e286e37bc6cd7ec35572637e3
Alon Stern <sternalon@gmail.com>
2017-03-17 13:44:32 -0400

    Updating Figure scripts to improve Figures

--- Scripts_for_paper_figures/Fig_Collapse_solo_melt.sh
+++ Scripts_for_paper_figures/Fig_Collapse_solo_melt.sh
@@ -1,2 +1,2 @@
 cd ../
-./static_shelf_solo.py -three_fields_flag=plot_melt_sst_ustar_berg -broken_shelf=True -save_figure=True  
+./static_shelf_solo.py -three_fields_flag=plot_melt_sst_ustar_berg -ylim_min=240. -ylim_max=440.0 -broken_shelf=True -use_Mixed_Melt=True -save_figure=True  
--- Scripts_for_paper_figures/Fig_spread_area_snapshots.sh
+++ Scripts_for_paper_figures/Fig_spread_area_snapshots.sh
@@ -1,2 +1,2 @@
 cd ../
-./snapshots.py -extension=icebergs_month.nc -plot_horizontal_field=True -use_ALE=True -time_ind1=14 -time_ind2=29 -time_ind3=59 -cmap=Blues -field=spread_area -vmin=0.0 -vmax=1.0  -use_days_title=False -mask_using_bergs=False -dir_slice_num=1 -colorbar_units='(non dim)' -save_figure=True
+./snapshots.py -extension=icebergs_month.nc -plot_horizontal_field=True -use_ALE=True -time_ind1=14 -time_ind2=29 -time_ind3=59 -cmap=Blues -field=spread_area -vmin=0.0 -vmax=1.0  -use_days_title=False -mask_using_bergs=False -dir_slice_num=1 -colorbar_units='(non dim)' -use_Mixed_Melt=True -save_figure=True
